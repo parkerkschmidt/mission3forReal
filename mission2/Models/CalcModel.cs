809ab19200440a85fa15417c8d9103ae571f1038
parkerkschmidt <parkerschmidt05@gmail.com>
2022-04-10 20:32:52 -0600

    mission 3

---
+++ mission2/Models/CalcModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission2.Models
+{
+    public class CalcModel
+    {
+        [Required (ErrorMessage = "Please enter the scores for assignments")]
+        [Range(1, 100)]
+        public float assign { get; set; }
+
+        [Required(ErrorMessage = "Please enter the scores for projects")]
+        [Range(1, 100)]
+        public float grp_proj { get; set; }
+
+        [Required(ErrorMessage = "Please enter the scores for quizzes")]
+        [Range(1, 100)]
+        public float quiz { get; set; }
+
+        [Required(ErrorMessage = "Please enter the scores for exams")]
+        [Range(1, 100)]
+        public float exam { get; set; }
+
+        [Required(ErrorMessage = "Please enter the scores for intex")]
+        [Range(1, 100)]
+        public float inte { get; set; }
+
+    }
+}
---
+++ mission2/wwwroot/js/calculator.js
@@ -0,0 +1,46 @@
+﻿// create function to calculate grade
+function myCalculator() {
+
+
+    //define variables using jquery, pulling in values from html form
+    let fAssignment = $('#assignment').val();
+    let fGrp_Project = $('#grp_project').val();
+    let fQuizzes = $('#quizzes').val();
+    let fExams = $('#exams').val();
+    let fIntex = $('#intex').val();
+
+    let fAssignWeight = .55;
+    let fGrpPrjWeight = .05;
+    let fQuizWeight = .10;
+    let fExamWeight = .20;
+    let fIntexWeight = .10;
+
+    let fFinalGrade = Math.round(((fAssignment * fAssignWeight) + (fGrp_Project * fGrpPrjWeight) + (fQuizzes * fQuizWeight) + (fExams * fExamWeight) + (fIntex * fIntexWeight)));
+    let sLetterGrade
+
+    // if statement to compare final grade % and assign letter grade
+    if (fFinalGrade >= 90) {
+        sLetterGrade = 'A';
+    }
+    else if (fFinalGrade >= 80) {
+        sLetterGrade = 'B';
+    }
+    else if (fFinalGrade >= 70) {
+        sLetterGrade = 'C';
+    }
+    else if (fFinalGrade >= 60) {
+        sLetterGrade = 'D';
+    }
+    else {
+        sLetterGrade = 'F';
+    }
+
+    // print out grades in alert
+    alert("Your grade is: " + "\n" + fFinalGrade + " " + sLetterGrade);
+
+}
+
+// on click, run the mycalculator function from above
+$("#calc_button").click(function () {
+    myCalculator();
+});
